7bf1e1c6d69b689736bd39feb9a91bf2d8578c5c
Shanmukha <shanmukharanganath@gmail.com>
2024-09-19 08:11:53 +0530

    setup github pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,11 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
+		adapter: adapter({ strict: false, fallback: '404.html' }),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+			base: process.env.NODE_ENV === 'production' ? '/medium-embed-image-generator' : '',
 		}
+
 	}
 };
 
